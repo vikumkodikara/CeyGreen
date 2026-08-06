9d2dada196dbc473128f8abb30b080f611c118cf
D.A.H. Pahasara De Silva <hiranyapahasara20@gmail.com>
2026-08-06 23:58:06 +0530

    feat(iot): add Device domain model for ESP32 nodes

---
+++ iot-service/src/main/java/com/ceygreen/iot/model/Device.java
@@ -0,0 +1,49 @@
+package com.ceygreen.iot.model;
+
+/**
+ * An ESP32 (or other edge device) attached to one zone.
+ * Stored under {@code /greenhouses/{id}/zones/{zoneId}/devices/{deviceId}}.
+ */
+public class Device {
+
+    private String deviceId;
+    private String deviceType;
+    private String deviceCode;
+
+    public Device() {
+    }
+
+    public Device(String deviceId, String deviceType, String deviceCode) {
+        this.deviceId = deviceId;
+        this.deviceType = deviceType;
+        this.deviceCode = deviceCode;
+    }
+
+    public static Device esp32(String deviceId, String deviceCode) {
+        return new Device(deviceId, "ESP32", deviceCode);
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceCode() {
+        return deviceCode;
+    }
+
+    public void setDeviceCode(String deviceCode) {
+        this.deviceCode = deviceCode;
+    }
+}
