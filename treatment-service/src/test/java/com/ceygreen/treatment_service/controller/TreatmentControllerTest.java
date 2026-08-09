e835380c3c574ee4d96a8d35073b0fbe474e3397
PasanAriyarathna <priyasanka19@gmail.com>
2026-08-09 08:07:18 +0530

    test(treatment-service): add TreatmentController MockMvc tests for all four endpoints

---
+++ treatment-service/src/test/java/com/ceygreen/treatment_service/controller/TreatmentControllerTest.java
@@ -0,0 +1,11 @@
+package com.ceygreen.treatment_service.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreatmentControllerTest {
+    @Test
+    void contextLoads() {
+    }
+}
