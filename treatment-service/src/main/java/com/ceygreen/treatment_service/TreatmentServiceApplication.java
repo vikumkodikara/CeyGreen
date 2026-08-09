59afb32446cc9c2288a4bb189bff3230b4046f54
PasanAriyarathna <priyasanka19@gmail.com>
2026-08-09 09:48:43 +0530

    Fix Treatment Service Maven dependencies, Spring Boot version mismatch, and Swagger 401 configuration

---
+++ treatment-service/src/main/java/com/ceygreen/treatment_service/TreatmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ceygreen.treatment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreatmentServiceApplication.class, args);
+	}
+
+}
